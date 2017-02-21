7177ff4c223eaa6ff229b180c5ac8374a77fb7a1
Ricardo Nazario <nazarioricardo@gmail.com>
2017-02-21 14:26:50 -0400

    New file structure, first GET

--- server.js
+++ server.js
@@ -2,15 +2,39 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const model = require('./models/index')
 
 const app = express()
 
 // Connect to database
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/blog')
+mongoose.connect('mongodb://localhost/hometurf') // Create database 
 
+// Configure static files (for handlebars)
+app.use(express.static('public')) // TODO: Public folder doesn't exist right now
 
+// Configure template engine for the server
+app.engine('handlebars', handlebars({
+    defaultLayout: 'main'
+}))
 
+app.set('view engine', 'handlebars')
+
+app.get('/community/:id/admin', function (req, res) {
+
+    // Community admin homepage
+    model.findById(req.params.id, function (err, article) {
+        if (err) {
+            return res.send(err)
+        }
+        return res.render('community-admin', model)
+    })
+})
+
+app.get('/communities', function (req, res) {
+    
+    // Get list of communities
+})
 
 app.listen(3000, function(req, res) {
     console.log('Server listening on port 3000')
